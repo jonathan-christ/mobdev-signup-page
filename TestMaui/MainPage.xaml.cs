ca093e4e40b424d21d6f8023b1434ddb39c2fd3d
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-02-21 20:44:53 +0800

    0.0.1 - Hypothetical Inserts for Sean

--- TestMaui/MainPage.xaml.cs
+++ TestMaui/MainPage.xaml.cs
@@ -9,12 +9,28 @@ public partial class MainPage : ContentPage // signup class
 
 	public bool ComparePassword()
 	{
-		return SignUpBtn.Equals(pass);
+		if (confPass != null && pass != null)
+		{
+			if (!string.IsNullOrEmpty(confPass.Text) && !string.IsNullOrEmpty(pass.Text))
+			{
+				return confPass.Text.Equals(pass.Text);
+			}
+			else
+			{
+				DisplayAlert("Empty fields!", "Make sure the password fields are not blank!", "OK");
+			}
+		}
+		else
+		{
+			DisplayAlert("Loading Error!", "Restart app!", "OK");
+		}
+
+		return false;
 	}
 
 	public User CreateUser()
 	{
-		return new User(fname, lname, pass);
+		return new User(fname.Text, lname.Text, pass.Text);
 	}
 
 	private void SignupUser(object sender, EventArgs e)
@@ -24,7 +40,9 @@ public partial class MainPage : ContentPage // signup class
 			User final = CreateUser();
 			Button button = (Button)sender;
 			DisplayAlert("Signed Up!", "You may now login!", "OK");
-		}else{
+		}
+		else
+		{
 			DisplayAlert("Passwords Error!", "Make sure your passwords match!", "OK");
 		}
 	}
